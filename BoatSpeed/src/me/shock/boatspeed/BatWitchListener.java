613e20f9907d9367e2d59545129363f1e5a5638c
Trent Hensler <thensler@wisc.edu>
2012-11-17 01:27:02 -0600

    Changing drops for witches

--- BoatSpeed/src/me/shock/boatspeed/BatWitchListener.java
+++ BoatSpeed/src/me/shock/boatspeed/BatWitchListener.java
@@ -1,11 +1,18 @@
 package me.shock.boatspeed;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.entity.Bat;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Witch;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BatWitchListener extends JavaPlugin implements Listener
@@ -18,13 +25,25 @@ public class BatWitchListener extends JavaPlugin implements Listener
 	}
 	
 	@EventHandler
-	public void onDeath(EntityDamageEvent event)
+	public void onDamage(EntityDamageEvent event)
 	{
 		Entity entity = event.getEntity();
 		if(entity instanceof Witch)
 		{
+			((Witch) entity).damage(100);
 			Location loc = entity.getLocation();
-			loc.getWorld().spawn(arg0, arg1);
+			loc.getWorld().spawn(loc, Bat.class);
+			Bukkit.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "Got witch damage event");
+		}
+	}
+	
+	@EventHandler
+	public void onDeath(EntityDeathEvent event)
+	{
+		Entity entity = event.getEntity();
+		if (entity instanceof Witch)
+		{
+			List<ItemStack> item = event.getDrops();
 		}
 	}
 }
